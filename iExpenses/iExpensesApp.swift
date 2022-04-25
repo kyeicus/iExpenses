9f4c91660417a8ab02034b5b0a946de579f923db
Emmanuel Kyei Nketia <eknketia001@st.ug.edu.gh>
2022-04-25 03:49:18 +0000

    Initial Commit

---
+++ iExpenses/iExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpensesApp.swift
+//  iExpenses
+//
+//  Created by Emmanuel K. Nketia on 4/25/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
